bb2f187030507f7c1d019da0ad6485d04a553db6
Gert Drapers <gert.drapers@live.com>
2021-10-06 22:02:29 -0700

    build & push action

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,17 +5,15 @@
 [ -z "${INPUT_VERBOSITY}" ] && INPUT_VERBOSITY="0"
 
 # validate if values are set
-[ -z "${INPUT_USERNAME}" ]  && echo "INPUT_USERNAME is not set exiting" && exit 2
-[ -z "${INPUT_PASSWORD}" ]  && exit "INPUT_PASSWORD is not set exiting" && exit 2
+[ -z "${INPUT_TAG}" ]       && echo "INPUT_TAG is not set exiting" && exit 2
 [ -z "${INPUT_SERVER}" ]    && exit "INPUT_SERVER is not set exiting" && exit 2
 [ -z "${INPUT_VERBOSITY}" ] && exit "INPUT_VERBOSITY is not set exiting" && exit 2
 
 # output all inputs env variables
-echo "POLICY-LOGIN        $(/app/policy version | sed 's/Policy CLI.//g')"
+echo "POLICY-PUSH         $(/app/policy version | sed 's/Policy CLI.//g')"
 printf "\n"
 printf "\n"
-echo "INPUT_USERNAME      ${INPUT_USERNAME}"
-echo "INPUT_PASSWORD      **********"
+echo "INPUT_TAG           ${INPUT_TAG}"
 echo "INPUT_SERVER        ${INPUT_SERVER}"
 echo "INPUT_VERBOSITY     ${INPUT_VERBOSITY}"
 echo "GITHUB_WORKSPACE    ${GITHUB_WORKSPACE}"
@@ -27,10 +25,13 @@ printf "\n"
 e_code=0
 
 # construct commandline arguments 
-CMD="/app/policy login --username=${INPUT_USERNAME} --password=${INPUT_PASSWORD} --server=${INPUT_SERVER} --verbosity=${INPUT_VERBOSITY}"
+CMD="/app/policy push ${INPUT_TAG}"
 
 # execute command
 eval "$CMD" || e_code=1
+printf "\n"
+
+/app/policy images --remote
 printf "\n"
 
 exit $e_code
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,42 @@
 #!/usr/bin/env bash
 
+[ -z "$1" ] && echo "provide container version" && exit 1
+
+CONTAINER=policy:$1
+
+# reset
 sudo rm -rf /tmp/policytest
+
+# init
+mkdir -p /tmp/policytest
+gh repo clone aserto-dev/policy-peoplefinder /tmp/policytest/peoplefinder
 
 docker run -ti \
 -e INPUT_USERNAME=gertd \
 -e INPUT_PASSWORD=${GIT_TOKEN} \
 -e INPUT_SERVER= \
 -e INPUT_VERBOSITY= \
+-e GITHUB_WORKSPACE=/github/workspace \
 -v "/tmp/policytest":"/github/workspace" \
 --entrypoint=/app/login.sh \
-policy:0.0.34-dirty 
+${CONTAINER}
 
+docker run -ti \
+-e INPUT_SRC=peoplefinder/src \
+-e INPUT_TAG=peoplefinder:$(sver -n patch) \
+-e INPUT_REVISION=$(sver) \
+-e INPUT_SERVER= \
+-e INPUT_VERBOSITY= \
+-e GITHUB_WORKSPACE=/github/workspace \
+-v "/tmp/policytest":"/github/workspace" \
+--entrypoint=/app/build.sh \
+${CONTAINER}
+
+docker run -ti \
+-e INPUT_TAG=peoplefinder:$(sver -n patch) \
+-e INPUT_SERVER= \
+-e INPUT_VERBOSITY= \
+-e GITHUB_WORKSPACE=/github/workspace \
+-v "/tmp/policytest":"/github/workspace" \
+--entrypoint=/app/push.sh \
+${CONTAINER}
