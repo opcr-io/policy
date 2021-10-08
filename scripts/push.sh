02489632023942f77a3e20227b74fd4df7eb9803
Gert Drapers <gert.drapers@live.com>
2021-10-07 20:25:46 -0700

    push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 # set defaults when not set
-[ -z "${INPUT_SERVER}" ]    && INPUT_SERVER="opcr.io"
 [ -z "${INPUT_VERBOSITY}" ] && INPUT_VERBOSITY="error"
 
 # validate if values are set
 [ -z "${INPUT_TAG}" ]       && echo "INPUT_TAG is not set exiting" && exit 2
-[ -z "${INPUT_SERVER}" ]    && exit "INPUT_SERVER is not set exiting" && exit 2
 [ -z "${INPUT_VERBOSITY}" ] && exit "INPUT_VERBOSITY is not set exiting" && exit 2
 
 VERBOSITY=0
@@ -30,7 +28,6 @@ echo "POLICY-PUSH         $(/app/policy version | sed 's/Policy CLI.//g')"
 printf "\n"
 printf "\n"
 echo "INPUT_TAG           ${INPUT_TAG}"
-echo "INPUT_SERVER        ${INPUT_SERVER}"
 echo "INPUT_VERBOSITY     ${INPUT_VERBOSITY} (${VERBOSITY})"
 printf "\n"
 
@@ -40,7 +37,7 @@ printf "\n"
 e_code=0
 
 # construct commandline arguments 
-CMD="/app/policy push ${INPUT_TAG} --server=${INPUT_SERVER} --verbosity=${VERBOSITY}"
+CMD="/app/policy push ${INPUT_TAG} --verbosity=${VERBOSITY}"
 
 # execute command
 eval "$CMD" || e_code=1
