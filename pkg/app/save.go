11ae9e94ba46278f3c848c2bad972b0ca7e70357
Gheorghe <gheorghe.popescu@aserto.com>
2021-10-08 17:42:10 +0300

    extract writePolicy

--- pkg/app/save.go
+++ pkg/app/save.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 	"oras.land/oras-go/pkg/content"
 )
@@ -34,13 +35,36 @@ func (c *PolicyApp) Save(userRef, outputFile string) error {
 		return errors.Errorf("policy [%s] not found in the local store", ref)
 	}
 
-	if outputFile != "-" {
+	if outputFile == "-" {
+		o = os.Stdout
+	} else {
 		c.UI.Normal().
 			WithStringValue("digest", refDescriptor.Digest.String()).
 			Msgf("Resolved ref [%s].", ref)
+		o, err = os.Create(outputFile)
+
+		if err != nil {
+			return errors.Wrapf(err, "failed to create output file [%s]", outputFile)
+		}
+
+		defer func() {
+			err := o.Close()
+			if err != nil {
+				c.UI.Problem().WithErr(err).Msg("Failed to close policy bundle tarball.")
+			}
+		}()
 	}
 
-	reader, err := ociStore.ReaderAt(c.Context, refDescriptor)
+	err = c.writePolicy(ociStore, &refDescriptor, o)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *PolicyApp) writePolicy(ociStore *content.OCIStore, refDescriptor *v1.Descriptor, o *os.File) error {
+	reader, err := ociStore.ReaderAt(c.Context, *refDescriptor)
 	if err != nil {
 		return errors.Wrap(err, "failed to open store reader")
 	}
@@ -49,22 +73,6 @@ func (c *PolicyApp) Save(userRef, outputFile string) error {
 		err := reader.Close()
 		if err != nil {
 			c.UI.Problem().WithErr(err).Msg("Failed to close OCI policy reader.")
-		}
-	}()
-
-	if outputFile == "-" {
-		o = os.Stdout
-	} else {
-		o, err = os.Create(outputFile)
-	}
-
-	if err != nil {
-		return errors.Wrapf(err, "failed to create output file [%s]", outputFile)
-	}
-	defer func() {
-		err := o.Close()
-		if err != nil {
-			c.UI.Problem().WithErr(err).Msg("Failed to close policy bundle tarball.")
 		}
 	}()
 
@@ -92,6 +100,5 @@ func (c *PolicyApp) Save(userRef, outputFile string) error {
 
 		i += chunkSize
 	}
-
 	return nil
 }
