79f4ace8e53bf0452cb8f7947c649da4075877bf
carabasdaniel <dani@aserto.com>
2023-03-20 11:27:50 +0200

    Show annotations from manifest in inspect
    Signed-off-by: carabasdaniel <dani@aserto.com>
    Signed-off-by: oanatmaria <oana@aserto.com>

--- pkg/app/inspect.go
+++ pkg/app/inspect.go
@@ -1,8 +1,12 @@
 package app
 
 import (
+	"bytes"
+	"encoding/json"
+
 	"github.com/opcr-io/policy/pkg/oci"
 	"github.com/opcr-io/policy/pkg/parser"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 )
 
@@ -23,20 +27,46 @@ func (c *PolicyApp) Inspect(userRef string) error {
 	if err != nil {
 		return errors.Wrapf(err, "failed to read content info for policy [%s]", ref)
 	}
-
 	c.UI.Normal().
 		WithStringValue("media type", contentInfo.MediaType).
 		WithStringValue("digest", contentInfo.Digest.String()).
 		WithIntValue("size", contentInfo.Size).
 		Do()
 
-	if len(contentInfo.Annotations) > 0 {
-		msg := c.UI.Normal().WithTable("Annotation", "Value")
-
-		for k, v := range contentInfo.Annotations {
-			msg.WithTableRow(k, v)
+	if contentInfo.MediaType == ocispec.MediaTypeImageManifest {
+		reader, err := ociClient.GetStore().Fetch(c.Context, contentInfo)
+		if err != nil {
+			return err
 		}
-		msg.Msg("Annotations")
+		manifestBytes := new(bytes.Buffer)
+		_, err = manifestBytes.ReadFrom(reader)
+		if err != nil {
+			return err
+		}
+		var manifest ocispec.Manifest
+		err = json.Unmarshal(manifestBytes.Bytes(), &manifest)
+		if err != nil {
+			return err
+		}
+
+		if len(manifest.Annotations) > 0 {
+			msg := c.UI.Normal().WithTable("Annotation", "Value")
+
+			for k, v := range manifest.Annotations {
+				msg.WithTableRow(k, v)
+			}
+			msg.Msg("Annotations")
+		}
+
+	} else {
+		if len(contentInfo.Annotations) > 0 {
+			msg := c.UI.Normal().WithTable("Annotation", "Value")
+
+			for k, v := range contentInfo.Annotations {
+				msg.WithTableRow(k, v)
+			}
+			msg.Msg("Annotations")
+		}
 	}
 
 	return nil
