5a75bf9abec811016d40f61bccce66a078f5aa31
Gert Drapers <1533850+gertd@users.noreply.github.com>
2026-03-14 11:30:03 +0100

    rework directory existance check on --untardir flag

--- cmd/policy/pull.go
+++ cmd/policy/pull.go
@@ -1,24 +1,37 @@
 package main
 
-import "github.com/pkg/errors"
+import (
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+type untarDir string
+
+func (d untarDir) AfterApply() error {
+	if fi, err := os.Stat(string(d)); err == nil && fi.IsDir() {
+		return nil
+	}
+
+	return errors.Errorf("--untardir directory %q does not exist", string(d))
+}
 
 type PullCmd struct {
 	Policies []string `arg:"" name:"policy" help:"Policies to pull from the remote registry."`
-	UntarDir string   `name:"untardir" type:"existingdir" help:"Directory to extract the policy bundle to (automatically extracts when set)."`
+	UntarDir untarDir `flag:"" name:"untardir"  help:"Extract the policy bundle to an existing directory."`
 }
 
 func (c *PullCmd) Run(g *Globals) error {
+	defer g.App.Context.Done()
+
 	var errs error
 
 	for _, policyRef := range c.Policies {
-		err := g.App.Pull(policyRef, c.UntarDir)
-		if err != nil {
+		if err := g.App.Pull(policyRef, string(c.UntarDir)); err != nil {
 			g.App.UI.Problem().WithErr(err).Msgf("Failed to pull policy: %s", policyRef)
 			errs = err
 		}
 	}
-
-	<-g.App.Context.Done()
 
 	if errs != nil {
 		return errors.Wrap(errs, "failed to pull one or more policies")
